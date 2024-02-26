82a6e986223e9d46b8d7e7aad84e4df0bea862cf
Juancho <ghware@users.noreply.github.com>
2024-02-26 19:58:10 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+Hooks.once("init", function() {
+  // Set the path to your translations directory here
+  Babele.get().setSystemTranslationsDir("../../modules/everyday-heroes-spanish-translation/compendium");
+});
